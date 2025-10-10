8420d3a2cec4acdcecd364fdb871154033885dae
MrBeckW <146685275+MrBeckW@users.noreply.github.com>
2025-10-09 18:29:44 -0700

    Update to player reset function.

--- Assets/Scripts/PlayerSpawnScript.cs
+++ Assets/Scripts/PlayerSpawnScript.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class PlayerSpawnScript : MonoBehaviour
 {
     [SerializeField] private GameObject player;
-    [SerializeField] private Rigidbody2D playerRBD;
+    [SerializeField] private GrappleGun grappleGun;
     [SerializeField] private Vector2 spawnPosition;
     [SerializeField] private Vector2 spawnVelocity;
 
     private PlayerInput playerInput;
+    private Rigidbody2D playerRBD;
 
     private void Start()
     {
@@ -20,13 +21,36 @@ public class PlayerSpawnScript : MonoBehaviour
         playerInput.Player.Respawn.performed += OnRespawn;
     }
 
+    /// <summary>
+    /// Event used for resetting player on player input.
+    /// </summary>
+    /// <param name="context"></param>
     private void OnRespawn(UnityEngine.InputSystem.InputAction.CallbackContext context)
+    {
+        ResetPlayer();
+    }
+
+    /// <summary>
+    /// Calls the functions required for resetting the player.
+    /// </summary>
+    public void ResetPlayer()
     {
         ResetPosition();
         ResetVelocity();
+        ResetGrapple();
     }
 
+    /// <summary>
+    /// resets the grapple gun
+    /// </summary>
+    private void ResetGrapple() { grappleGun.StopGrappling(); }
+    /// <summary>
+    /// resets the position of the player
+    /// </summary>
     private void ResetPosition() { player.transform.position = spawnPosition; }
+    /// <summary>
+    /// resets the velocity of the player.
+    /// </summary>
     private void ResetVelocity() { playerRBD.linearVelocity = spawnVelocity; }
 
 }
